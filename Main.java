9d8780e9d7382ff67db1cb543b48630af5207cdf
UnrulySystem <aidan_g@CodingFort.localdomain>
2026-09-24 10:15:43 -0700

    Second edit

--- Main.java
+++ Main.java
@@ -1,10 +1,13 @@
+
+
 public class Main
 {
 	public static void main(String[] args) {
 		//print "Hello GitHub"
-
+		System.out.println("Hello, Github");
 		//print your name
-
+		System.out.println("Aidan G");
 		//make any other edits to this file you want, get creative!
+		System.out.println("Git is all good and evil")
 	}
 }
